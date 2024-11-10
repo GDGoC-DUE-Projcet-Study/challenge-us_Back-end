1dd219825211ce0fa7a35f52f835e926ef037ba7
leejyohan <chyun1529@gmail.com>
2024-11-10 22:11:58 +0900

    🎨 Improve : 스키마 예시 추가

--- api/user_schema.py
+++ api/user_schema.py
@@ -7,11 +7,38 @@ class CreateUser(BaseModel):
     name: str
     phone:str
 
+    class Config:
+        json_schema_extra = {
+            "example" : {
+                "id" : "아이디",
+                "pw" : "비번",
+                "name": "이름",
+                "phone" : "010-1234-1234"
+            }
+        } 
+
 class DeleteUser(BaseModel):
     id: str
     pw: str
+
+    class Config:
+        json_schema_extra = {
+            "example" : {
+                "id" : "아이디",
+                "pw" : "비번"
+            }
+        } 
 
 class UpdateUser(BaseModel):
     pw: str
     name: str
     phone:str
+
+    class Config:
+        json_schema_extra = {
+            "example" : {
+                "pw" : "비번",
+                "name": "이름",
+                "phone" : "010-1234-1234"
+            }
+        } 
